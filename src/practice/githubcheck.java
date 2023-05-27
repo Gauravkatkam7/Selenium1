6dedfc5bf7b30d0d5be217bdf3fda8f64fb3e726
Gaurav <Gaurav@DESKTOP-6NU7MHK>
2023-05-27 13:09:03 +0530

    [gaurav]: new githubcheck class created

---
+++ src/practice/githubcheck.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class githubcheck {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi github here");
+		
+		
+		System.out.println("hello eclipse here");
+	}
+}
